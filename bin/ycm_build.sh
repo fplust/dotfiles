2398b708e21642f5b21438fdbdcce88156fe3af3
FPlust Lu <fplustlu@gmail.com>
2016-04-28 19:59:01 +0800

    fix ycmbuild script error

--- bin/ycm_build.sh
+++ bin/ycm_build.sh
@@ -1,2 +1,2 @@
-cmake -G Unix Makefiles -DPATH_TO_LLVM_ROOT=~/ycmtemp/clang+llvm-3.8.0-x86_64-opensuse13.2 -DUSE_PYTHON2=OFF . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/clang+llvm-3.8.0-x86_64-opensuse13.2 -DUSE_PYTHON2=OFF . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_core
